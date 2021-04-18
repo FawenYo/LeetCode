3433a91641dfaab51aa5de95f631afae4ec0f1ba
FawenYo <twsky020600@gmail.com>
2021-04-18 15:04:29 +0800

    finalize questions auto fetching

--- question.py
+++ question.py
@@ -1,22 +1,39 @@
 import os
+import sys
+import subprocess
+import pkg_resources
 import re
-from typing import List
+from typing import List, Tuple
+
+
+# Check if required packages installed
+required = {"html2markdown", "rich", "requests"}
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = required - installed
+
+if missing:
+    print(f"Installing missing packages...")
+    python = sys.executable
+    subprocess.check_call(
+        [python, "-m", "pip", "install", *missing], stdout=subprocess.DEVNULL
+    )
+    print("Packages installed!")
 
 import html2markdown
+import rich, rich.progress, rich.table, rich.console
 import requests
 
 
 class Question:
-    def __init__(self) -> None:
-        self.slug: str = ""  # LeetCode question slug
+    def __init__(self, slug: str) -> None:
+        self.slug: str = slug  # LeetCode question slug
         self.folder_path: str = ""  # Folder path
         self.question_data: dict = {}  # LeetCode question data
         self.question_content: str = (
             ""  # LeetCode question content (str, Markdown format)
         )
 
-    def get_problem_content(self, slug: str) -> None:
-        print(f"{slug} fetching...")
+    def get_problem_content(self) -> None:
         self.fetch_question()
 
         try:
@@ -29,14 +46,12 @@ class Question:
         # Question python3 file
         self.create_solution_py()
 
-        print("done!")
-
     def fetch_question(self) -> None:
         """Fetch question data using graghql"""
         url = "https://leetcode.com/graphql"
         json = {
             "operationName": "questionData",
-            "variables": {"titleSlug": slug},
+            "variables": {"titleSlug": self.slug},
             "query": """query questionData($titleSlug: String!) {
                 question(titleSlug: $titleSlug) {
                     questionId
@@ -96,7 +111,7 @@ class Question:
     def create_question_md(self) -> None:
         """Create QUESTION.MD for question info"""
         with open(f"{self.folder_path}/QUESTION.MD", mode="w+") as question_file:
-            question_url = f"https://leetcode.com/problems/{slug}/"
+            question_url = f"https://leetcode.com/problems/{self.slug}/"
             title = f"# [{self.question_data['questionId']}. {self.question_data['title']}]({question_url}) - {self.question_data['difficulty']}"
             markdown_content = self.adjust_markdown(
                 markdown_content=self.question_content
@@ -113,8 +128,11 @@ class Question:
             with open(
                 f"{self.folder_path}/{python_filename}", mode="x"
             ) as question_file:
-                code = self.python_snippets(
-                    codeSnippets=self.question_data["codeSnippets"]
+                code = (
+                    'import sys\n\nsys.path.append(".")\nfrom model import *\n\n\n'
+                    + self.python_snippets(
+                        codeSnippets=self.question_data["codeSnippets"]
+                    )
                 )
                 question_file.writelines(code)
         except FileExistsError:
@@ -122,9 +140,69 @@ class Question:
 
 
 if __name__ == "__main__":
-    problems = [
-        "Merge Two Binary Trees",
-    ]
-    for question in problems:
-        slug = question.lower().replace(" ", "-")
-        Question().get_problem_content(slug=slug)
+    DATE = "2021-04-25"  # End date of the work. Format: "%Y-%m-%d".
+    OPTIONAL = True  # Fetch optional question. Defaults to True.
+
+    def fetch_questions(date: str) -> Tuple[List[str]]:
+        """Fetch week questions from server
+
+        Args:
+            date (str): End date of work
+
+        Returns:
+            Tuple[List[str]]: ([required_questions], [optional_questions])
+        """
+        rich.print(f":truck: Fetching {DATE} questions from server...")
+        server_url = f"https://leetcode-bot.ml/get_question?date={date}"
+        response = requests.get(url=server_url).json()
+
+        if response["code"] == 200:
+            rich.print(":o: Successfully fetched questions list!")
+            return (response["required_questions"], response["optional_questions"])
+        else:
+            problems = ([], [])
+            rich.print(f":x: {response['error']['message']}")
+        return problems
+
+    def show_questions(
+        required_questions: List[str], optional_questions: List[str]
+    ) -> None:
+        """Show week questions
+
+        Args:
+            required_questions (List[str]): Required questions
+            optional_questions (List[str]): Optional questions
+        """
+        table = rich.table.Table(title=f"{DATE} 題目")
+        table.add_column("類型", justify="right", style="green", no_wrap=True)
+        table.add_column("題目名稱", style="magenta")
+        table.add_column("題目網址", justify="right", style="blue")
+
+        for question in required_questions:
+            info, slug = question.split("__||__")
+            table.add_row("必寫", info, f"https://leetcode.com/problems/{slug}/")
+
+        for question in optional_questions:
+            info, slug = question.split("__||__")
+            table.add_row("選寫", info, f"https://leetcode.com/problems/{slug}/")
+
+        rich.console.Console().print(table)
+
+    required_questions, optional_questions = fetch_questions(date=DATE)
+
+    problems = (
+        (required_questions + optional_questions) if OPTIONAL else required_questions
+    )
+    if problems:
+        for question in rich.progress.track(
+            problems, description=":page_with_curl: Fetching question info..."
+        ):
+            info, slug = question.split("__||__")
+            Question(slug=slug).get_problem_content()
+        rich.print(":white_check_mark: Job done!")
+
+        show_questions(
+            required_questions=required_questions, optional_questions=optional_questions
+        )
+    else:
+        exit()
